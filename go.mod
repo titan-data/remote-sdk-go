30d6cb9e1654a4b8aa03080b2b7313831a139306
Eric Schrock <Eric.Schrock@delphix.com>
2020-01-09 09:34:45 -0500

    beginning of gRPC implementation

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/titan-data/remote-sdk-go
 
 require (
+	github.com/golang/protobuf v1.3.2
 	github.com/hashicorp/go-hclog v0.10.1
 	github.com/hashicorp/go-plugin v1.0.1
 	github.com/stretchr/testify v1.4.0
+	google.golang.org/grpc v1.26.0
 )
 
 go 1.13
--- internal/echo/echo_test.go
+++ internal/echo/echo_test.go
@@ -9,20 +9,20 @@ import (
 func TestType(t *testing.T) {
 	r := EchoRemote{}
 	typ, _ := r.Type()
-	assert.Equal(t, "mock", typ)
+	assert.Equal(t, "echo", typ)
 }
 
 func TestToURL(t *testing.T) {
 	r := EchoRemote{}
 	u, props, _ := r.ToURL(map[string]interface{}{"a": "b"})
-	assert.Equal(t, "mock://mock", u)
+	assert.Equal(t, "echo://echo", u)
 	assert.Len(t, props, 1)
 	assert.Equal(t, "b", props["a"])
 }
 
 func TestFromURL(t *testing.T) {
 	r := EchoRemote{}
-	u, _ := url.Parse("mock://mock")
+	u, _ := url.Parse("echo://echo")
 	res, _ := r.FromURL(u, map[string]string{"a": "b"})
 	assert.Len(t, res, 1)
 	assert.Equal(t, "b", res["a"])
